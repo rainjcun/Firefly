f8415ba35116af6f980cecdb1bf92988afc97190
fqzlr <a10275974@outlook.com>
2026-04-26 17:59:10 +0800

    尝试添加友联自动化

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -1,7 +1,5 @@
 import type { FriendLink, FriendsPageConfig } from "../types/config";
 
-// 可以在src/content/spec/friends.md中编写友链页面下方的自定义内容
-
 // 友链页面配置
 export const friendsPageConfig: FriendsPageConfig = {
 	title: "",
@@ -24,8 +22,8 @@ export const friendsConfig: FriendLink[] = [
 	},
 	{
 		title: "椰汁の主页",
-		imgurl: "https://free.picui.cn/free/2026/03/23/23/69c12fe83f7a4.jpg",
-		desc: "关关难过关关过，前路漫漫亦灿灿。",
+		imgurl: "https://free.picui.cn/free/2026/03/23/69c12fe83f7a4.jpg",
+		desc: "关关难过关关过,前路漫漫亦灿灿.",
 		siteurl: "https://home.132614.xyz/",
 		tags: ["Blog"],
 		weight: 10,
@@ -41,7 +39,7 @@ export const friendsConfig: FriendLink[] = [
 		enabled: true,
 	},
 
-	// ↓↓ 自动化友链写入位置 ↓↓
+	// ↓↓ 自动化友链写入位置（请勿手动修改） ↓↓
 ];
 
 // 获取启用的友链并进行排序

a01aabcb1cc9ac753606f67ada218fa56c4352df
fqzlr <a10275974@outlook.com>
2026-06-01 18:01:58 +0800

    朋友

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -258,6 +258,16 @@ export const friendsConfig: FriendLink[] = [
 		enabled: true, // 是否启用
 		issue_id: 50,
 	},
+		{
+		title: "L!!!!ght",
+		imgurl: "https://easyimg.kejk.cn/i/4484873c-c2cc-4b3d-bc35-5c72ed01cfd9.webp",
+		desc: "阳光正好，慢慢前行。",
+		siteurl: "https://sunlight.kejk.cn/",
+		tags: ["Blog"],
+		weight: 10, // 权重，数字越大排序越靠前
+		enabled: true, // 是否启用
+		issue_id: 50,
+	},
 ];
 
 // 获取启用的友链并进行排序

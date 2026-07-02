51775690a0fe8b6e9218f17d5465db65ad764f38
rainjcun <1296016282@qq.com>
2026-07-02 22:54:49 +0800

    fix: 优化移动端响应式适配，修复iOS Safari和Android Chrome兼容性问题

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/posts/mdx-example.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmdx-example.mdx&astroContentModuleFlag=true")],
+["src/content/spec/friends.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fspec%2Ffriends.mdx&astroContentModuleFlag=true")]]);
+		
